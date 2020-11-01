03dde31e1d21dde1d165b8ece011ca08976d57b1
query-set <j3k.walczak@gmail.com>
2020-11-01 16:16:46 +0100

    Update custom.el and .gitignore

--- custom.el
+++ custom.el
@@ -3,12 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-abort-manual-when-too-short t)
- '(company-dabbrev-downcase nil)
- '(company-show-numbers t)
- '(company-tooltip-limit 20)
+ '(custom-safe-themes
+   '("b5fff23b86b3fd2dd2cc86aa3b27ee91513adaefeaa75adc8af35a45ffb6c499" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" default))
  '(package-selected-packages
-   '(restclient doom-themes all-the-icons doom-modeline ein blacken py-autopep8 magit docker-tramp sql-indent avy flycheck smooth-scrolling mood-one-theme mood-line json-mode which-key exec-path-from-shell elpy swiper flx-ido use-package)))
+   '(git-identity cider jedi multiple-cursors restclient doom-themes all-the-icons doom-modeline ein blacken py-autopep8 magit docker-tramp avy json-mode which-key exec-path-from-shell elpy swiper flx-ido use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
